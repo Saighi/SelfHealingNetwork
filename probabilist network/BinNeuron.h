a6ae8a6780219d9111abb4e2bd3fd28865f92af0
Paul saighi <paulsaighi@hotmail.com>
2023-09-06 12:35:37 +0200

    two_pool

--- probabilist network/BinNeuron.h
+++ probabilist network/BinNeuron.h
@@ -1,6 +1,10 @@
 #ifndef BINNEURON_H
 #define BINNEURON_H
 
+#include <vector>
+
+using namespace std;
+
 class BinNeuron
 {
 private :
@@ -9,15 +13,14 @@ private :
     int id;
 
 public:
-    
-    double inhibActivity;
-    double state;
-    double pastState;
-    double pastPastState;
+
+    int state;
+    int pastState;
+    int pastPastState;
+    vector<int> history;
+    vector<int> historyTimeStamp;
 
     BinNeuron(){
-
-        inhibActivity = 0;
         id = nextId++;
         state = 0;
         pastState = 0;
@@ -26,6 +29,16 @@ public:
 
     int getID() const {
         return id;
+    }
+
+    void SetState(int newState){
+        state = newState;
+    }
+
+    // Doesn't have any time stamp, should/could maybe change 
+    void registerHistory(int timeStamp){
+        history.push_back(state);
+        historyTimeStamp.push_back(timeStamp);
     }
 };
 
--- probabilist network/Synapse.h
+++ probabilist network/Synapse.h
@@ -9,12 +9,25 @@ public:
     double weight;
     BinNeuron* sourceNeuron;
     BinNeuron* targetNeuron;
+    vector<double> history;
+    vector<int> historyTimeStamp;
 
     Synapse(float weight, BinNeuron* sourceNeuron, BinNeuron* targetNeuron) 
         : weight(weight), sourceNeuron(sourceNeuron), targetNeuron(targetNeuron) {}
 
     Synapse()
         : weight(0), sourceNeuron(nullptr), targetNeuron(nullptr) {}
+
+    void setWeight(double newWeight){
+        weight = newWeight;
+    }
+
+    
+    void registerHistory(int timeStamp){
+        history.push_back(weight);
+        historyTimeStamp.push_back(timeStamp);
+    }
+
 };
 
 #endif
